8eaf2125efc3ea598996881723d58c111ee406c9
marioluja <mario4education@gmail.com>
2022-08-17 00:09:27 -0700

    Laying out the dashboard and testing the "add class" section.

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,16 +1,48 @@
 import  created_account  from "./sign_up";
 import React from 'react';
-import {useContext} from 'react';
+import {useContext,useRef} from 'react';
 import {StudentContext} from './Objects/StudentContext';
 
 
 
 export default function dashboard()
 {
+    let course = {
+        name: "nil",
+        grade: 0
+    };
+
+    let course_name= useRef(null);
+
+    function add_classlist()
+    {
+        alert(course_name.current.value);
+    }
+
     const{student, set_student}= useContext(StudentContext);
+    var student_obj = JSON.parse(student);
     return(
         <div className="dashboard-container">
-            <h1>{student}</h1>
+            <h2 className="greeting">Welcome First name</h2>
+            <div className="class_headers">
+                <div className="class_name">
+                    <h3>class name</h3>
+                </div>
+                <div className="current_grade">
+                    <h3>current grade</h3>
+                </div>
+            </div>
+            <div className="classes">
+                <div className="classes_scroll">
+                </div>
+            </div>
+            <div className="add_class">
+                <div className="add_class_input">
+                    <p>Class Name:</p>
+                    <input type="text" className="class_name_input" placeholder="Class Name" ref={course_name}/>
+                </div>
+                <button type="button" className="submit_class" onClick={add_classlist}>Add Class</button>
+            </div>
         </div>
     )
 }
